2fbae08e4312f9961a82786bf7f3eebf0c9cf029
moja8 <moja888@gmail.com>
2012-01-02 01:48:53 +0900

    output red/green

--- chap1/test/strftime_test.js
+++ chap1/test/strftime_test.js
@@ -1,6 +1,17 @@
-var date = new Date(2009, 11, 5);
+var date = new Date(2009, 9, 2);
 
-console.log(date.strftime("%Y"));
-console.log(date.strftime("%m"));
-console.log(date.strftime("%d"));
-console.log(date.strftime("%y"));
+try {
+  assert("%Y should return full year",
+      date.strftime("%Y") === "2009");
+  assert("%m should return month",
+      date.strftime("%m") === "10");
+  assert("%d should return date",
+      date.strftime("%d") === "02");
+  assert("%y should return year as two digits",
+      date.strftime("%y") === "09");
+  assert("%F should return acts as %Y-%m-%d",
+      date.strftime("%F") === "2009-10-02");
+  output(assert.count + " tests OK", "#0c0");
+} catch (e) {
+  output("Test failed: " + e.message, "#c00");
+}
